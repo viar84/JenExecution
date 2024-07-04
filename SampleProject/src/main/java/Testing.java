13750d7e4e7d8d8db3b6b428ae5872f15c7151c9
vijayalakshmi <58000247+viar84@users.noreply.github.com>
2024-07-04 13:41:50 +0530

    Update Testing.java

--- SampleProject/src/main/java/Testing.java
+++ SampleProject/src/main/java/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
- //test - updtate for test1 branch - update for test1-2nd time
+ //test - updtate for test1 branch - update for test1-2nd time -change from git
 	}
 
 }
